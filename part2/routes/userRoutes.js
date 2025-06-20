5452a5f4b6f1ae1071c2e5d60f25ffeacfc53718
Eswar Chodavarapu <84902477+Eswar2607@users.noreply.github.com>
2025-06-20 19:31:38 +0000

    [automatic commit] 2025-06-20 19:31:37 UTC 193.115.120.78

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -37,7 +37,15 @@ router.get('/me', (req, res) => {
 });
 
 router.get('/dogs', async (req, res) => {
-  
+  if (!req.session.user || req.session.user.role !== 'owner') {
+        return res.status(401).json({ error: 'Not authorized or not logged in' });
+    }
+
+    try {
+      const own
+    } catch (error) {
+
+    }
 })
 
 // POST login (dummy version)
