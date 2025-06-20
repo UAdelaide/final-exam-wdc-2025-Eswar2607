a4e38c1575c34c7eded7cd46cc644b085dfcd9f3
Eswar Chodavarapu <84902477+Eswar2607@users.noreply.github.com>
2025-06-20 19:19:34 +0000

    [automatic commit] 2025-06-20 19:19:34 UTC 193.115.120.78

--- part2/app.js
+++ part2/app.js
@@ -66,7 +66,7 @@ app.use('/api/users', userRoutes);
 app.post('/logout', (req, res) => {
     req.session.destroy(err => {
         if (err) {
-            return res.status(500).json.
+            return res.status(500).json({message:'Couldn't })
         }
     })
 })
