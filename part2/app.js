46b8bd25287479079e423b5f6cf52595c469bd32
Eswar Chodavarapu <84902477+Eswar2607@users.noreply.github.com>
2025-06-20 19:18:13 +0000

    [automatic commit] 2025-06-20 19:18:12 UTC 193.115.120.78

--- part2/app.js
+++ part2/app.js
@@ -31,38 +31,38 @@ app.use('/api/users', userRoutes);
 
 // Export the app instead of listening here
 
-app.post('/login', async (req, res) => {
-    try {
-        const { username, password } = req.body;
-        const [rows] = await dbConnection.execute(
-            'SELECT * FROM Users WHERE username = ? AND password_hash = ?', [username, password]);
+// app.post('/login', async (req, res) => {
+//     try {
+//         const { username, password } = req.body;
+//         const [rows] = await dbConnection.execute(
+//             'SELECT * FROM Users WHERE username = ? AND password_hash = ?', [username, password]);
 
-        if (rows.length === 1) {
-            const user = rows[0];
+//         if (rows.length === 1) {
+//             const user = rows[0];
 
-            req.session.user = {
-                id: user.user_id,
-                username: user.username,
-                role: user.role
-            };
+//             req.session.user = {
+//                 id: user.user_id,
+//                 username: user.username,
+//                 role: user.role
+//             };
 
-            if (user.role === 'owner') {
-                res.redirect('/owner-dashboard.html');
-            } else if (user.role === 'walker') {
-                res.redirect('/walker-dashboard.html');
-            } else {
-                res.status(403).send('Role not supported for login');
-            }
-        } else {
-            res.status(401).send('Invalid username or password');
-        }
-    } catch (error) {
-        console.error('Login Error:', error);
-        res.status(500).send('An error occurred during the login process.');
-    }
-});
+//             if (user.role === 'owner') {
+//                 res.redirect('/owner-dashboard.html');
+//             } else if (user.role === 'walker') {
+//                 res.redirect('/walker-dashboard.html');
+//             } else {
+//                 res.status(403).send('Role not supported for login');
+//             }
+//         } else {
+//             res.status(401).send('Invalid username or password');
+//         }
+//     } catch (error) {
+//         console.error('Login Error:', error);
+//         res.status(500).send('An error occurred during the login process.');
+//     }
+// });
 
 
-app.post
+app.post('/logout', ())
 
 module.exports = app;
