37ad1638dd7f8aa2d13dc1dad6b667545c24ddbd
Eswar Chodavarapu <84902477+Eswar2607@users.noreply.github.com>
2025-06-20 11:17:22 +0000

    [automatic commit] 2025-06-20 11:17:22 UTC 193.115.120.78

--- part1/app.js
+++ part1/app.js
@@ -72,7 +72,20 @@ app.get('/api/dogs', async (req, res) => {
 
 app.get('/api/walkrequests/open', async (req, res) => {
     try {
-        const [rows] = await dbConnection.execute
+        const [rows] = await dbConnection.execute(
+            `SELECT
+            WalkRequests.request_id,
+            Dogs.name AS dog_name,
+            WalkRequests.requested_time,
+            WalkRequests.duration_minutes,
+            WalkRequests.location,
+            Users.username AS owner_username
+        FROM WalkRequests
+        JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
+        JOIN Users ON Dogs.owner_id = Users.user_id
+        WHERE WalkRequests.status = 'open'
+        ORDER BY WalkRequests.requested_time ASC`
+        )
     } catch (error) {
 
     }
