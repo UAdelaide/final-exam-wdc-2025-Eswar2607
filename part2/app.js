10159868db265c8a29c768d19f361bcbf44f0888
Eswar Chodavarapu <84902477+Eswar2607@users.noreply.github.com>
2025-06-20 18:49:12 +0000

    [automatic commit] 2025-06-20 18:49:12 UTC 193.115.120.78

--- part2/app.js
+++ part2/app.js
@@ -37,6 +37,7 @@ app.post('/login', async (req, res) => {
                 username: user.username,
                 role: user.role
             };
+            
             if (user.role === 'owner') {
                 res.redirect('/owner-dashboard.html');
             } else if (user.role === 'walker') {
