9b9c7c3a2c4d3ed3f92d7ff8e59a4ca2674abb63
Eswar Chodavarapu <84902477+Eswar2607@users.noreply.github.com>
2025-06-20 10:56:10 +0000

    [automatic commit] 2025-06-20 10:56:10 UTC 193.115.120.78

--- part1/app.js
+++ part1/app.js
@@ -25,7 +25,7 @@ app.use('/users', usersRouter);
             host: 'localhost',
             user:'root',
             password:'',
-            database:'Dogs'
+            database:'DogWalkService'
         })
     }
 })
